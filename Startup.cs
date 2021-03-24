a474949d724aea59e49120e56409384b5d71fc1d
bfank9@gmail.com <bfank9@gmail.com>
2021-03-23 22:50:50 -0600

    controller changed in startup

--- Startup.cs
+++ Startup.cs
@@ -59,7 +59,7 @@ namespace IS_413_Assignment_3
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=NewMovies}/{action=Create}/{id?}");
             });
         }
     }
